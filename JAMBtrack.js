335677dddccf3ddd63f59c90b23b96d6558cbe74
liyalawal <victorialawal3.0@gmail.com>
2024-08-06 16:22:38 +0100

    Add files via upload
    project for javascript

---
+++ JAMBtrack.js
@@ -0,0 +1,391 @@
+
+let jamb=[
+    {
+        "course": "Business Administration",
+        "subjects": ["Use of English", "Mathematics", "Economics", "any other Social Science subject"]
+    },
+    {
+        "course": "Public Administration",
+        "subjects": ["Use of English", "Government", "Economics", "any other subject"]
+    },
+    {
+        "course": "Banking and Finance",
+        "subjects": ["Use of English", "Mathematics", "one Social Science subject", "any other subject"]
+    },
+    {
+        "course": "Economics",
+        "subjects": ["Use of English", "Mathematics", "Economics", "any of Government, History, Geography, Literature in English, French, CRK/IRK"]
+    },
+    {
+        "course": "Demography and Social Statistics",
+        "subjects": ["Use of English", "Mathematics", "Economics/ Geography", "any other subject"]
+    },
+    {
+        "course": "Geography",
+        "subjects": ["Use of English", "Geography", "two other Arts or Social Science subjects"]
+    },
+    {
+        "course": "Library Science",
+        "subjects": ["Use of English", "Any three Arts or Social Science subjects"]
+    },
+    {
+        "course": "Mass Communication",
+        "subjects": ["Use of English", "any three from Arts or Social Science subjects"]
+    },
+    {
+        "course": "Sociology",
+        "subjects": ["Use of English", "Three Social Science or Arts subjects"]
+    },
+    {
+        "course": "Political Science",
+        "subjects": ["Use of English", "Government or History", "two other Social Science/Arts subjects"]
+    },
+    {
+        "course": "Philosophy",
+        "subjects": ["Use of English", "Government", "any other two subjects"]
+    },
+    {
+        "course": "Psychology",
+        "subjects": ["Use of English", "Any three subjects from Arts or Social Science"]
+    },
+    {
+        "course": "Religious Studies",
+        "subjects": ["Use of English Language", "CRK/IRS", "any two other subjects"]
+    },
+    {
+        "course": "Social Works",
+        "subjects": ["Use of English Language", "Mathematics", "Economics/Geography", "any other subject"]
+    },
+    {
+        "course": "Accountancy",
+        "subjects": ["Use of English", "Mathematics", "Economics", "any other Social Science subject"]
+    },
+    {
+        "course": "Sociology and Anthropology",
+        "subjects": ["Use of English", "Three Social Science or Arts Subjects"]
+    },
+    {
+        "course": "Industrial Relations",
+        "subjects": ["Use of English", "Mathematics", "Economics", "one other relevant subject"]
+    },
+    {
+        "course": "Human Resources Management",
+        "subjects": ["Use of English", "Economics", "Government", "any other relevant subjects"]
+    },
+    {
+        "course": "International Relations",
+        "subjects": ["Use of English", "Economics", "Literature in English", "Geography/Government/History"]
+    },
+    {
+        "course": "Business Management",
+        "subjects": ["Use of English", "Mathematics", "Economics", "one other subject"]
+    },
+    {
+        "course": "Cooperative and Rural Development",
+        "subjects": ["Use of English", "Mathematics", "Economics", "one other subject"]
+    },
+    {
+        "course": "Tourism",
+        "subjects": ["English", "Mathematics", "Economics", "any other subject"]
+    },
+    {
+        "course": "Marketing",
+        "subjects": ["Use of English", "Mathematics", "Economics", "one other relevant Subject"]
+    },
+    {
+        "course": "Insurance",
+        "subjects": ["English", "Mathematics", "Economics", "one other subject"]
+    },
+    {
+        "course": "Food, Science and Technology",
+        "subjects": ["Use of English", "Chemistry", "Mathematics / Physics", "Agric Science"]
+    },
+    {
+        "course": "Pharmacy",
+        "subjects": ["Use of English", "Biology", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Industrial Chemistry",
+        "subjects": ["Use of English", "Chemistry", "Mathematics", "any of Physics/Biology/Agricultural Science"]
+    },
+    {
+        "course": "Fisheries",
+        "subjects": ["Use of English", "Chemistry", "Biology/Agricultural Science", "any other Science subject"]
+    },
+    {
+        "course": "Geology",
+        "subjects": ["Use of English", "any three of Chemistry, Physics, Mathematics, Biology, Geography"]
+    },
+    {
+        "course": "Geography",
+        "subjects": ["Use of English", "Geography", "any two of Mathematics, Biology, Chemistry, Physics, Agricultural Science"]
+    },
+    {
+        "course": "Medicine and Surgery",
+        "subjects": ["Use of English", "Biology", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Agricultural Engineering",
+        "subjects": ["Use of English", "Mathematics", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Computer Science",
+        "subjects": ["Use of English", "Mathematics", "Physics", "one of Biology, Chemistry, Agric Science, Economics, Geography"]
+    },
+    {
+        "course": "Biochemistry",
+        "subjects": ["Use of English", "Biology", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Biological Sciences",
+        "subjects": ["Use of English", "Biology", "Chemistry", "Physics or Mathematics"]
+    },
+    {
+        "course": "Physics",
+        "subjects": ["Use of English", "Physics", "Mathematics", "Chemistry or Biology"]
+    },
+    {
+        "course": "Mathematics",
+        "subjects": ["Use of English", "Mathematics", "any two of Physics, Chemistry, Economics, Biology, Agricultural Science"]
+    },
+    {
+        "course": "Chemistry",
+        "subjects": ["Use of English", "Chemistry", "two of Physics, Biology, Mathematics"]
+    },
+    {
+        "course": "Nursing",
+        "subjects": ["Use of English", "Physics", "Biology", "Chemistry"]
+    },
+    {
+        "course": "Surveying and Geoinformatics",
+        "subjects": ["Use of English", "Physics", "Mathematics", "any of Chemistry, Geography, Art, Biology, Economics"]
+    },
+    {
+        "course": "Statistics",
+        "subjects": ["Use of English", "Mathematics", "any two of Physics"]
+    },
+    {
+        "course": "Building",
+        "subjects": ["Use of English", "Physics", "Mathematics", "any of Chemistry, Geography, Art, Biology, Economics"]
+    },
+    {
+        "course": "Microbiology",
+        "subjects": ["Use of English", "Biology", "Chemistry", "either Physics or Mathematics"]
+    },
+    {
+        "course": "Veterinary Science",
+        "subjects": ["English", "Physics", "Chemistry", "Biology"]
+    },
+    {
+        "course": "Forestry",
+        "subjects": ["Use of English", "Chemistry", "Biology or Agriculture", "Physics or Mathematics"]
+    },
+    {
+        "course": "Civil Engineering",
+        "subjects": ["Use of English", "Mathematics", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Chemical Engineering",
+        "subjects": ["Use of English", "Mathematics", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Computer Engineering",
+        "subjects": ["Use of English", "Mathematics", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Electrical Engineering",
+        "subjects": ["Use of English", "Mathematics", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Estate Management",
+        "subjects": ["Use of English", "Mathematics", "Economics", "one other subject"]
+    },
+    {
+        "course": "Anatomy",
+        "subjects": ["English", "Mathematics", "Biology", "Chemistry or Physics"]
+    },
+    {
+        "course": "Dentistry",
+        "subjects": ["Use of English", "Chemistry", "Biology", "one Science subject"]
+    },
+    {
+        "course": "Medical Laboratory Science",
+        "subjects": ["English Language", "Physics", "Chemistry", "Biology"]
+    },
+    {
+        "course": "Medical Rehabilitation",
+        "subjects": ["Use of English", "Physics", "Chemistry", "Biology"]
+    },
+    {
+        "course": "Physiology",
+        "subjects": ["Use of English", "Biology", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Electronic Engineering",
+        "subjects": ["Use of English", "Mathematics", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Marine Engineering",
+        "subjects": ["Use of English", "Mathematics", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Mechanical Engineering",
+        "subjects": ["Use of English", "Mathematics", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Metallurgical and Materials Engineering",
+        "subjects": ["Use of English", "Mathematics", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Petroleum and Gas Engineering",
+        "subjects": ["Use of English", "Mathematics", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Systems Engineering",
+        "subjects": ["Use of English", "Mathematics", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Structural Engineering",
+        "subjects": ["Use of English", "Mathematics", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Production and Industrial Engineering",
+        "subjects": ["Use of English", "Mathematics", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Architecture",
+        "subjects": ["English", "Physics", "Mathematics", "any of Chemistry, Geography, Art, Biology, Economics"]
+    },
+    {
+        "course": "Quantity Surveying",
+        "subjects": ["Use of English", "Physics", "Mathematics", "any of Chemistry, Geography, Art, Biology, Economics"]
+    },
+    {
+        "course": "Urban and Regional Planning",
+        "subjects": ["English", "Mathematics", "Geography", "one of Economics, Physics, Chemistry"]
+    },
+    {
+        "course": "Physiotherapy",
+        "subjects": ["Use of English", "Biology", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Radiography",
+        "subjects": ["Use of English", "Biology", "Physics", "Chemistry"]
+    },
+    {
+        "course": "Veterinary Medicine",
+        "subjects": ["Use of English", "Biology", "Physics", "Chemistry"]
+    },
+    {
+        "course": "French",
+        "subjects": ["English", "French", "any other two subjects from Arts and Social Sciences"]
+    },
+    {
+        "course": "English Language",
+        "subjects": ["Use of English", "Lit.-in-English", "one other Arts subject", "another Arts or Social Science subject"]
+    },
+    {
+        "course": "Hausa",
+        "subjects": ["English", "Hausa", "Lit in English", "any of Economics, Government, History, Arabic"]
+    },
+    {
+        "course": "History and International Studies",
+        "subjects": ["Use of English", "History/Government", "any other two subjects from Arts & Social Science"]
+    },
+    {
+        "course": "Islamic Studies",
+        "subjects": ["English", "Islamic Religious Studies", "two other Arts subjects"]
+    },
+    {
+        "course": "Igbo",
+        "subjects": ["English", "Igbo", "two subjects from Arts and social Sciences"]
+    },
+    {
+        "course": "Mass Communication",
+        "subjects": ["Use of English", "Any three Arts and Social Science subjects"]
+    },
+    {
+        "course": "Music",
+        "subjects": ["Use of English", "Music", "one other Arts subject", "any other subject"]
+    },
+    {
+        "course": "Philosophy",
+        "subjects": ["Use of English", "Any three subjects"]
+    },
+    {
+        "course": "Arabic and Islamic Studies",
+        "subjects": ["Use of English", "Arabic", "Two subjects from Arts and/or Social Sciences"]
+    },
+    {
+        "course": "Christian Religious Studies",
+        "subjects": ["Use of English", "Two Arts subjects including Christian Religious Knowledge", "any other subject"]
+    },
+    {
+        "course": "Fine and Applied Arts",
+        "subjects": ["Use of English Language", "Fine Art", "two other Arts subjects or Social Science subject"]
+    },
+    {
+        "course": "Theatre Arts",
+        "subjects": ["Use of English", "Lit. in English", "two other relevant subjects"]
+    },
+    {
+        "course": "Linguistics",
+        "subjects": ["Use of English", "Two relevant Arts subjects", "any other subject"]
+    },
+    {
+        "course": "English and International Studies",
+        "subjects": ["Use of English", "Literature in English", "Government or History", "any other Arts subjects"]
+    },
+    {
+        "course": "Religious Studies",
+        "subjects": ["English", "CRK/IRS", "any two other subjects"]
+    },
+    {
+        "course": "Yoruba",
+        "subjects": ["Use of English", "Yoruba", "two other subjects in Arts or Social Sciences"]
+    },
+    {
+        "course": "Anthropology",
+        "subjects": ["Use of English", "Any three of History, CRK/IRK, Geography, Economics, Literature in English, French"]
+    },
+    {
+        "course": "Criminology and Security Studies",
+        "subjects": ["English", "Economics", "Government", "any one of the following: History, Geography, Literature in English, French, IRK, Hausa"]
+    },
+    {
+        "course": "Law",
+        "subjects": ["English", "Literature", "Economics", "any other Art Subject"]
+    },
+    {
+        "course": "Civil Law",
+        "subjects": ["English Language", "Any three Arts or Social Science subjects"]
+    },
+    {
+        "course": "Islamic / Sharia Law",
+        "subjects": ["Use of English Language", "Any three Arts or Social Science subjects including Arabic or Islamic Studies"]
+    }
+]
+let button=document.getElementsByTagName('button')[2]
+let input=document.getElementsByTagName('input')[0]
+console.log(button)
+let input2=prompt('')
+// alert(input)
+
+
+
+button.addEventListener('click', function (){
+    let courseName=new RegExp(input,'i');
+    for(let count=0;count<jamb.length;count++){
+        if(jamb[count].course.search(courseName)!=-1){
+            alert(jamb[count].subjects+ '\n Do you want to see more options ' );
+          
+        }
+    }
+})
+
+    // console.log(jamb[count].subjects)
+    
+
+
+
+
